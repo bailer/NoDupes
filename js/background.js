f83688fa9f3b6a49ea634026d4483714ca2e2a20
Jakob Sennerby <jakob.sennerby@gmail.com>
2016-04-05 15:06:29 +0200

    initiating the repo

---
+++ js/background.js
@@ -0,0 +1,95 @@
+var itemsByNotificationId = {};
+var itemsByExistingId = {};
+var searchedItems = {};
+
+chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, suggest) {
+  var searchFilter = {
+    state: "complete",
+    fileSize: downloadItem.fileSize,
+    exists: true, 
+    filenameRegex: downloadItem.filename
+  };
+  chrome.downloads.search(searchFilter, function (downloadItems) {
+    if (downloadItems.length > 0) {
+      console.log(downloadItems);
+      itemsByExistingId[downloadItems[0].id] = {exists: true};
+      console.log("Current item " + downloadItems[0].id + " exists: " + downloadItems[0].exists);
+      chrome.notifications.create({
+        type: "basic",
+        iconUrl: "images/icon.png",
+        title: "File already exists",
+        message: "It seems like the file " + downloadItem.filename + " already exists",
+        contextMessage: "Click to show the file",
+        isClickable: true,
+        buttons: [
+          {title: "Open existing file", iconUrl: "images/open.png"},
+          {title: "Download anyway", iconUrl: "images/download.png"}
+        ]
+      }, function (notificationId) {
+        console.log("created notification with id " + notificationId);
+        if (itemsByExistingId[downloadItems[0].id].exists) {
+          itemsByNotificationId[notificationId] = {foundItem: downloadItems[0], downloadItem: downloadItem, suggest: suggest};
+          itemsByExistingId[downloadItems[0].id] = {notificationId: notificationId, suggest: suggest};
+        } else {
+          console.log("doesnt exist anymore");
+          chrome.notifications.clear(notificationId);
+          suggest();
+        }
+      });
+    } else {
+      suggest();
+    }
+  });
+  return true;
+});
+
+chrome.downloads.onChanged.addListener(function (downloadDelta) {
+  console.log(downloadDelta);
+  if (downloadDelta.exists && downloadDelta.exists.previous === true && downloadDelta.exists.current === false) {
+    console.log("a file was removed");
+    if (itemsByExistingId[downloadDelta.id]) {
+      itemsByExistingId[downloadDelta.id] = {exists: false};
+    }
+  }
+});
+
+chrome.notifications.onClosed.addListener(function (notificationId, byUser) {
+  if (itemsByNotificationId[notificationId]) {
+    console.log(itemsByNotificationId[notificationId]);
+    // if (byUser) {
+      chrome.downloads.cancel(itemsByNotificationId[notificationId].downloadItem.id);
+      chrome.downloads.erase({id: itemsByNotificationId[notificationId].downloadItem.id});
+    // } else {
+      // itemsByNotificationId[notificationId].suggest();
+    // }
+    delete itemsByExistingId[itemsByNotificationId[notificationId].foundItem.id];
+    delete itemsByNotificationId[notificationId];
+  }
+});
+
+chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
+  if (itemsByNotificationId[notificationId]) {
+    if (buttonIndex === 0) {
+      chrome.notifications.clear(notificationId);
+      chrome.downloads.cancel(itemsByNotificationId[notificationId].downloadItem.id);
+      chrome.downloads.erase({id: itemsByNotificationId[notificationId].downloadItem.id});
+      chrome.downloads.open(itemsByNotificationId[notificationId].foundItem.id);
+    } else if (buttonIndex === 1) {
+      chrome.notifications.clear(notificationId);
+      itemsByNotificationId[notificationId].suggest();
+    }
+    delete itemsByExistingId[itemsByNotificationId[notificationId].foundItem.id];
+    delete itemsByNotificationId[notificationId];
+  }
+});
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+  if (itemsByNotificationId[notificationId]) {
+    chrome.notifications.clear(notificationId);
+    chrome.downloads.cancel(itemsByNotificationId[notificationId].downloadItem.id);
+    chrome.downloads.erase({id: itemsByNotificationId[notificationId].downloadItem.id});
+    chrome.downloads.show(itemsByNotificationId[notificationId].foundItem.id);
+    delete itemsByExistingId[itemsByNotificationId[notificationId].foundItem.id];
+    delete itemsByNotificationId[notificationId];
+  }
+});
